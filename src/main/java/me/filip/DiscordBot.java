b66de2c0ee88ccde288c9ad0cf048334eb8cf33f
Zeladd <filippesteam@gmail.com>
2022-10-15 02:00:22 +0300

    Add $ SHUTDOWN method

--- src/main/java/me/filip/DiscordBot.java
+++ src/main/java/me/filip/DiscordBot.java
@@ -1,6 +1,7 @@
 package me.filip;
 
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.requests.GatewayIntent;
 import net.dv8tion.jda.api.JDABuilder;
 
 import javax.security.auth.login.LoginException;
@@ -9,10 +10,9 @@ public class DiscordBot {
 
 	public static void main(String[] args) throws LoginException {
 		
-		Token token = Token.getToken();
-		
 		JDA bot = JDABuilder.createDefault
-				(token.toString())
+				(Config.get("token"))
+				.enableIntents(GatewayIntent.MESSAGE_CONTENT)
 				.addEventListeners(new Listener())
 				.build();
 
--- src/main/java/me/filip/Listener.java
+++ src/main/java/me/filip/Listener.java
@@ -2,6 +2,7 @@ package me.filip;
 
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 import javax.annotation.Nonnull;
 
@@ -17,4 +18,17 @@ public class Listener extends ListenerAdapter {
 		LOGGER.info("{} is ready", event.getJDA().getSelfUser().getAsTag());
 	}
 	
+	@Override
+	public void onMessageReceived(@Nonnull MessageReceivedEvent event) {
+		String prefix = Config.get("prefix");
+		String raw = event.getMessage().getContentRaw();
+		
+		if (event.isFromGuild()) {
+			if (raw.equals(prefix + "SHUTDOWN") && 
+					event.getAuthor().getId().equals(Config.get("owner_id"))) {
+				LOGGER.info("Shutting down!");
+				event.getJDA().shutdown();
+			}
+		}
+	}
 }
